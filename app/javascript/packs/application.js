5e235b48686a0609397aadbcf972d96ddfdc0812
makoto <makoto@users.noreply.github.com>
2020-12-30 18:30:42 +0900

    続きへ機能

--- app/javascript/packs/application.js
+++ app/javascript/packs/application.js
@@ -8,6 +8,7 @@ require("@rails/ujs").start()
 require("@rails/activestorage").start()
 require("channels")
 require('jquery')
+require('read_next')
 
 
 // Uncomment to copy all static images under ../images to the output folder and reference
